591f136d6590e5b395a561b4869b3a677b252267
osama_mohamed98 <om107547@gmail.com>
2022-02-07 13:28:42 +0300

    fix refresh indicator

---
+++ lib/feature/view_model/product_view_model/product_view_model.dart
@@ -0,0 +1,20 @@
+import 'package:get/get.dart';
+
+class ProductViewModel extends GetxController{
+
+
+  @override
+  void onInit() {
+    super.onInit();
+  }
+
+  @override
+  void onClose() {
+
+  }
+
+  @override
+  void onReady() {
+
+  }
+}
---
+++ lib/network/api/feature/product_helper.dart
@@ -0,0 +1,43 @@
+import 'package:inbox_clients/feature/model/my_order/order_sales.dart';
+import 'package:inbox_clients/network/api/model/app_response.dart';
+import 'package:inbox_clients/network/api/model/order_api.dart';
+import 'package:inbox_clients/network/api/model/product.dart';
+import 'package:inbox_clients/network/utils/constance_netwoek.dart';
+import 'package:logger/logger.dart';
+
+class ProductHelper {
+  ProductHelper._();
+
+  static final ProductHelper getInstance = ProductHelper._();
+  var log = Logger();
+
+  Future getAllProduct({required int pageSize, required int page}) async {
+    var appResponse = await ProductApi.getInstance.getMyProducts(
+        queryParameters: {
+          "${ConstanceNetwork.page}": "$page",
+          "${ConstanceNetwork.pageSize}": "$pageSize"
+        },
+        url: "${ConstanceNetwork.allOrder}",
+        header: ConstanceNetwork.header(4));
+    if (appResponse.status?.success == true) {
+      List data = appResponse.data["items"];
+      return data/*data.map((e) => ProductSales.fromJson(e)).toSet()*/;
+    } else {
+      return {};
+    }
+  }
+
+
+  Future getProductDetails({required String productId}) async {
+    var appResponse = await ProductApi.getInstance.getProductDetails(
+        // body: body,
+        url: "${ConstanceNetwork.orderDetails}?${ConstanceNetwork.productId}=$productId",
+        header: ConstanceNetwork.header(4));
+    if (appResponse.status?.success == true) {
+      Logger().e(appResponse.data["items"]);
+      return appResponse.data["items"] /*ProductSales.fromJson(appResponse.data["order"])*/;
+    } else {
+      return appResponse.data;
+    }
+  }
+}
