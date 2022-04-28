11175f41002185d55f0de41dcd14ce9bb7f04430
monther <m.abuabedallah@ahdtech.com>
2022-04-28 11:03:05 +0300

    Hot Fixess (Fix Close Icon)

--- lib/feature/view/screens/storage/new_storage/widgets/add_storage_widget/bulk_item_widget.dart
+++ lib/feature/view/screens/storage/new_storage/widgets/add_storage_widget/bulk_item_widget.dart
@@ -61,7 +61,10 @@ class BulkItemWidget extends StatelessWidget {
                 onTap: () {
                   deleteFunction();
                 },
-                child: SvgPicture.asset("assets/svgs/close.svg")))
+                child: SvgPicture.asset("assets/svgs/close.svg" , 
+                height: sizeH20,
+                width: sizeW20,
+                )))
       ],
     );
   }
